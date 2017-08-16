6e1d7ef84ecbb85844992a93a6cb326a8ab0a59d
dholland <dholland@LTS486X>
2017-08-16 14:12:07 -0500

    Add project files.

---
+++ PC-Info/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPCinfo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.txtHostnameOrIP = New System.Windows.Forms.TextBox()
+        Me.lblHost = New System.Windows.Forms.Label()
+        Me.rtbLog = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(258, 10)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 1
+        Me.btnGo.Text = "&Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'txtHostnameOrIP
+        '
+        Me.txtHostnameOrIP.Location = New System.Drawing.Point(100, 12)
+        Me.txtHostnameOrIP.Name = "txtHostnameOrIP"
+        Me.txtHostnameOrIP.Size = New System.Drawing.Size(152, 20)
+        Me.txtHostnameOrIP.TabIndex = 0
+        Me.txtHostnameOrIP.Text = "lts486x"
+        '
+        'lblHost
+        '
+        Me.lblHost.AutoSize = True
+        Me.lblHost.Location = New System.Drawing.Point(11, 15)
+        Me.lblHost.Name = "lblHost"
+        Me.lblHost.Size = New System.Drawing.Size(83, 13)
+        Me.lblHost.TabIndex = 2
+        Me.lblHost.Text = "Hostname or IP:"
+        '
+        'rtbLog
+        '
+        Me.rtbLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.rtbLog.Font = New System.Drawing.Font("Lucida Console", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rtbLog.Location = New System.Drawing.Point(11, 58)
+        Me.rtbLog.Name = "rtbLog"
+        Me.rtbLog.Size = New System.Drawing.Size(809, 323)
+        Me.rtbLog.TabIndex = 2
+        Me.rtbLog.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Output window:"
+        '
+        'frmPCinfo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(831, 393)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.rtbLog)
+        Me.Controls.Add(Me.lblHost)
+        Me.Controls.Add(Me.txtHostnameOrIP)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "frmPCinfo"
+        Me.Text = "PC Info"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents txtHostnameOrIP As TextBox
+    Friend WithEvents lblHost As Label
+    Friend WithEvents rtbLog As RichTextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ PC-Info/Form1.vb
@@ -0,0 +1,201 @@
+﻿' Simple project used to collect info from remote computer using WMI
+
+
+Public Class frmPCinfo
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        ' Run each Sub to collect info
+        Win32_OperatingSystem(txtHostnameOrIP.Text)
+        Win32_Processor(txtHostnameOrIP.Text)
+        Win32_BIOS(txtHostnameOrIP.Text)
+        Win32_ComputerSystem(txtHostnameOrIP.Text)
+        Win32_LogicalDisk(txtHostnameOrIP.Text)
+        Win32_NetworkAdapterConfiguration(txtHostnameOrIP.Text)
+
+    End Sub
+
+    Private Sub Win32_OperatingSystem(ByVal strHostname As String)
+
+        Dim objWMIService As Object = GetObject("winmgmts:\\" & strHostname & "\root\cimv2")
+        Dim colItems = objWMIService.ExecQuery("Select * from Win32_OperatingSystem")
+        Dim strOSname As String = Nothing
+
+        For Each objItem In colItems
+            strOSname = objItem.Name.ToString()
+            If strOSname <> Nothing Then Exit For
+        Next
+
+        ' Remove the drive partition info
+        If strOSname.Contains("|") Then
+            Dim arrSplit() As String = Split(strOSname, "|")
+            strOSname = arrSplit(0)
+        End If
+
+        Log("OS: " & strOSname)
+
+    End Sub
+
+    Private Sub Win32_Processor(ByVal strHostname As String)
+
+        Dim objWMIService As Object = GetObject("winmgmts:\\" & strHostname & "\root\cimv2")
+        Dim colItems = objWMIService.ExecQuery("Select * from Win32_Processor")
+        Dim strCPU As String = Nothing
+
+        For Each objItem In colItems
+            strCPU = objItem.Name.ToString
+            If strCPU <> Nothing Then Exit For
+        Next
+
+        Log("CPU: " & strCPU)
+    End Sub
+
+    Private Sub Win32_BIOS(ByVal strHostname As String)
+
+        Dim objWMIService As Object = GetObject("winmgmts:\\" & strHostname & "\root\cimv2")
+        Dim colItems = objWMIService.ExecQuery("Select * from Win32_BIOS")
+        Dim strSerialNumber As String = Nothing
+        Dim strBIOSversion As String = Nothing
+
+        For Each objItem In colItems
+            strSerialNumber = objItem.SerialNumber.ToString
+            strBIOSversion = objItem.SMBIOSBIOSVersion.ToString
+        Next
+
+        Log("BIOS SR#: " & strSerialNumber)
+        Log("BIOS Ver: " & strBIOSversion)
+
+    End Sub
+
+    Private Sub Win32_ComputerSystem(ByVal strHostname As String)
+        Dim objWMIService As Object = GetObject("winmgmts:\\" & strHostname & "\root\cimv2")
+        Dim colItems = objWMIService.ExecQuery("Select * from Win32_ComputerSystem")
+
+        Dim strModel As String = Nothing
+        Dim strName As String = Nothing
+        Dim strMemory As String = Nothing
+        Dim strUID As String = Nothing
+
+        For Each objItem In colItems
+
+            strModel = objItem.Model.ToString
+            strName = objItem.Name.ToString
+            strMemory = objItem.TotalPhysicalMemory.ToString
+            strUID = objItem.UserName.ToString
+
+        Next
+
+        If strMemory > Nothing Then
+            Dim intMemory As Long = CLng(strMemory)
+            strMemory = FormatNumber((intMemory / 1024 / 1024), 0) & "Gb"
+        End If
+
+        Log("Model: " & strModel)
+        Log("Name: " & strName)
+        Log("RAM: " & strMemory)
+        Log("Username: " & strUID)
+
+    End Sub
+
+    Private Sub Win32_LogicalDisk(ByVal strHostname As String)
+        Dim objWMIService As Object = GetObject("winmgmts:\\" & strHostname & "\root\cimv2")
+        Dim colItems = objWMIService.ExecQuery("Select * from Win32_LogicalDisk")
+
+        For Each objItem In colItems
+            Try
+                Log(("Drive: " & objItem.Caption.ToString).ToString.PadRight(8) &
+                 (" | Size: " & ReformatNumber(objItem.Size.ToString)).ToString.PadRight(18) &
+                 (" | Free: " & ReformatNumber(objItem.FreeSpace.ToString)).ToString.PadRight(18) &
+                 (" | Type: " & objItem.Description).ToString)
+            Catch ex As Exception
+                Log("Error in Private Sub Win32_LogicalDisk.  Error: " & ex.Message)
+            End Try
+        Next
+    End Sub
+
+    Private Sub Win32_NetworkAdapterConfiguration(ByVal strHostname As String)
+        Dim objWMIService As Object = GetObject("winmgmts:\\" & strHostname & "\root\cimv2")
+        Dim colItems = objWMIService.ExecQuery("Select * from Win32_NetworkAdapterConfiguration Where IPEnabled=TRUE")
+
+        Dim strDHCPenabled As String
+        Dim strDHCPServer As String
+        Dim strIPAddress As String
+        Dim strMacAddress As String
+
+        For Each objItem In colItems
+            strDHCPenabled = " | DHCP enabled: " & objItem.DHCPEnabled
+            strDHCPServer = " | DHCP server: " & objItem.DHCPServer.ToString
+            If objItem.DHCPServer.ToString.Trim = String.Empty Then strDHCPServer = " | DHCP server: n/a"
+
+            If objItem.IPAddress.Length >= 1 Then
+                For i = LBound(objItem.IPAddress) To UBound(objItem.IPAddress)
+                    strIPAddress = "IP: " & objItem.IPAddress(i)
+                    strMacAddress = " | MAC: " & objItem.MACAddress(i)
+                    Log(strIPAddress.ToString.PadRight(29) &
+                        strMacAddress.ToString.PadRight(19) &
+                        strDHCPenabled.ToString.PadRight(22) &
+                        strDHCPServer)
+                Next
+            End If
+        Next
+    End Sub
+
+    ' Reformat WMI Date/Time to normal Date/Time (not used in this app)
+    Private Function WMIDateStringToDate(ByVal dtmDate)
+        Dim dtWMIDateStringToDate = ""
+
+        If dtmDate.ToString = Nothing Then
+            Return ""
+        Else
+            ' Replace any * with zeros
+            If InStr(dtmDate, "*") > 0 Then Replace(dtmDate, "*", "0")
+
+            Try
+                If dtmDate.ToString > "" Then
+                    dtWMIDateStringToDate = CDate(Mid(dtmDate, 5, 2) & "/" &
+                    Mid(dtmDate, 7, 2) & "/" & Strings.Left(dtmDate, 4) _
+                    & " " & Mid(dtmDate, 9, 2) & ":" & Mid(dtmDate, 11, 2) & ":" & Mid(dtmDate, 13, 2))
+                End If
+            Catch ex As Exception
+                WMIDateStringToDate = ""
+            End Try
+        End If
+
+        Return dtWMIDateStringToDate
+    End Function
+
+    ' Reformat RAM and HDD sizes to Gb/Mb
+    Public Shared Function ReformatNumber(ByVal InComingNumber As String)
+        Dim strNumberToReformat As String = InComingNumber
+        Dim NumberToReformat As Single
+
+        If strNumberToReformat = "" Then strNumberToReformat = "0"
+
+        NumberToReformat = CLng(strNumberToReformat)
+
+        If NumberToReformat < 899999 Then    ' Kb
+            NumberToReformat = NumberToReformat / 1024
+            NumberToReformat = (FormatNumber(NumberToReformat, 3))
+            strNumberToReformat = CStr(NumberToReformat) & " Kb"
+        ElseIf NumberToReformat < 899999999 Then 'Mb
+            NumberToReformat = NumberToReformat / 1024000
+            NumberToReformat = (FormatNumber(NumberToReformat, 2))
+            strNumberToReformat = CStr(NumberToReformat) & " Mb"
+        ElseIf NumberToReformat < 899999999999 Then    ' Gb
+            NumberToReformat = NumberToReformat / 1024000000
+            NumberToReformat = (FormatNumber(NumberToReformat, 1))
+            strNumberToReformat = CStr(NumberToReformat) & " Gb"
+        ElseIf NumberToReformat < 899999999999999 Then   ' Tb
+            NumberToReformat = NumberToReformat / 1024000000000
+            NumberToReformat = (FormatNumber(NumberToReformat, 1))
+            strNumberToReformat = CStr(NumberToReformat) & " Tb"
+        End If
+
+        Return strNumberToReformat
+    End Function
+
+    ' "Log" is easier than "rtbLog.AppendText(" every time
+    Private Sub Log(ByVal strMessage As String)
+        rtbLog.AppendText(strMessage & vbNewLine)
+    End Sub
+
+End Class
